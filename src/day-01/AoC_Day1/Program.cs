6da7eb28c4e8ef4fabbe6148fd7881675f88bf22
nevoroman <nevoroman@gmail.com>
2021-12-01 19:40:19 +0300

    Day 1 solution added

---
+++ src/day-01/AoC_Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+// Put your input to "input.txt" file
+var depths = File.ReadAllLines("input.txt").Select(int.Parse).ToList();
+
+var answer1 = depths.Skip(1).Where((depth, i) => depth > depths[i]).Count();
+
+var answer2 = depths.Skip(3).Where((depth, i) => 
+    depth + depths[i + 2] + depths[i + 1] > depths[i] + depths[i + 2] + depths[i + 1]
+).Count();
+
+Console.WriteLine($"First puzzle answer: {answer1}, second puzzle answer: {answer2}");
